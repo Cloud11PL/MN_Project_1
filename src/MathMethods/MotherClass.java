69391d653494da7c175baa02c347e51ba9f59203
Mike <uaxau@hotmail.com>
2019-01-12 05:43:53 +0100

    Mother Class 0.1sec KeyFrame and 1 sec KeyFrame + Lock on fuel burn span(dont know if good tho)

--- src/MathMethods/MotherClass.java
+++ src/MathMethods/MotherClass.java
@@ -1,6 +1,14 @@
 package MathMethods;
 
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.input.KeyEvent;
+import javafx.util.Duration;
+
+import java.util.concurrent.atomic.AtomicReference;
+
 public class MotherClass {
 
     Overkill mass = new Mass();
@@ -15,8 +23,11 @@ public class MotherClass {
     private double fuelBurn0 = 0;
     private double m = massFuel0 + 1000;
     private double acc = accelerate.variable(m, fuelBurn0);
-//    private ArrayClass ac = new ArrayClass();
-
+    private ArrayClass ac = new ArrayClass();
+    private Timeline timeline = new Timeline();
+    private KeyEvent event;
+    private KeyFrame getNewVal = new KeyFrame(Duration.millis(1000), actionEvent -> fuelBurnInc());
+    private KeyFrame countNewVal = new KeyFrame(Duration.millis(100), e -> doImportantStuff(fuelBurnInc(), ac));
 
     public void doImportantStuff(double fuelBurn, ArrayClass ac) {
 
@@ -34,8 +45,28 @@ public class MotherClass {
 
     }
 
+     public double fuelBurnInc(){
+        if(fuelBurn0 <=0 &&fuelBurn0 >=- 16.5) {
+            switch(event.getCode()) {
+                case D: fuelBurn0 += -0.5;
+                case A: fuelBurn0 -= -0.5;
+            }
+        }
+        return fuelBurn0;
+     }
+
+     public void myTime(){
+
+        timeline.getKeyFrames().addAll(getNewVal,countNewVal);
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+     }
+
+     public void breakingAnimation(){
+        timeline.pause();
+     }
 //
-//        do {
+//    do {
 //    } while (h = 0) {
 //        if (v =< 2){
 //            System.out.println("Win");
