aa9d298179b65d339e9641ce1ddbcd840a183725
Mike <uaxau@hotmail.com>
2019-01-22 21:14:00 +0100

    Prolly good counting

--- src/MathMethods/MotherClass.java
+++ src/MathMethods/MotherClass.java
@@ -32,18 +32,19 @@ public class MotherClass {
     }
 
     public void doImportantStuff(double fuelBurn, ArrayClass ac) {
-
         ac.update(h,m,v,acc);
-
-        double mNew = mass.variable(m, fuelBurn);
-        double vNew = velocity.variable(v, acc);
-        double hNew = height.variable(h, v);
-        double accNew = accelerate.variable(m, fuelBurn);
+        double tStep = 0.01;
+        double mNew = m+tStep*fuelBurn*10;
+        double vNew = v + acc*tStep*10;
+        double hNew = h + v/10;
+        double gravity = 1.63;
+        double k = 636;
+        double acceleration = -gravity - k*fuelBurn/m;
 
         m = mNew;
         v = vNew;
-        h = hNew;
-        acc = accNew;
+        h =  hNew;
+        acc = acc*tStep + acceleration;
 
     }
 }
